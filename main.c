c6a884bd606b2e4cb2dcf7c46cd8fee885af45a7
Sankarshan DH <sankarshan.dh.2003@gmail.com>
2023-04-17 19:47:07 +0530

    some_changes

--- main.c
+++ main.c
@@ -1,17 +1,17 @@
  //Timing Calculations:
   /*
   Horizontal timing:
-  Visible area - 320 counts
-  Front porch - 16 counts
-  Sync pulse-51.2(51)
-  Back porch - 35 counts
-  Whole line 422 counts
+  Visible area - 240 counts
+  Front porch - 12 counts
+  Sync pulse-38
+  Back porch - 26 counts
+  Whole line 316 counts
   Vertical Timing(lines):
   Visible area- 600 lines
   Front porch 1 lines
   Sync pulse 4 lines
   Back porch 23 lines
-  Whole frame 632 lines
+  Whole frame 628 lines
   Info From Datasheet:
   Timer1A CLK i/p on P3.7
 */
@@ -62,8 +62,23 @@ int main( void )
   WDTCTL = WDTPW + WDTHOLD;
 
   //Caliberate DCO for 16MHz
-  DCOCTL=CALDCO_16MHZ;
-  BCSCTL1=CALBC1_16MHZ;
+   CSCTL0_H = CSKEY_H;         // unlock CS registers
+   CSCTL1 = DCOFSEL_4;         // set DCO frequency range to 16MHz
+   CSCTL2 = SELA__LFXTCLK | SELS__DCOCLK | SELM__DCOCLK; // set clock sources
+   CSCTL3 = DIVA__1 | DIVS__1 | DIVM__1;                // set clock dividers
+   CSCTL4 &= ~LFXTOFF;         // turn on LFXT crystal oscillator
+      do {
+          CSCTL5 &= ~LFXTOFFG;    // clear LFXT oscillator fault flag
+          SFRIFG1 &= ~OFIFG;      // clear oscillator fault flag
+      } while (SFRIFG1 & OFIFG);  // wait for LFXT oscillator to stabilize
+   CSCTL0_H = 0;               // lock CS registers
+
+      // Set up DCO for sub-main clock calibration
+   CSCTL0_H = CSKEY_H;         // unlock CS registers
+   CSCTL1 = DCOFSEL_3;         // set DCO frequency range to 12MHz
+   CSCTL2 = SELA__LFXTCLK | SELS__DCOCLK | SELM__DCOCLK; // set clock sources
+   CSCTL3 = DIVA__1 | DIVS__1 | DIVM__1;                // set clock dividers
+   CSCTL0_H = 0;
   //Setting up Timer A
   TACTL = TACLR + TASSEL_0 + MC_1 + ID_0+TAIE;
   //Setting up port P1.2 for HSync
@@ -88,7 +103,7 @@ int main( void )
   CCTL2=CCIE;
   //Configuring timer 2
   TA1CTL = TASSEL_2+MC_1+TACLR+ID_0+TAIE;
-  TA1CCR0=422;
+  TA1CCR0=316;
   TA1CCTL0=CCIE;
   TA1CCR1=336;
   TA1CCTL1=CCIE;
